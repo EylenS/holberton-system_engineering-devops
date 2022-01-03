00126025db6ad9a446fc09fc327ae7fa76aa5ca6
eylens <eymasac@gmail.com>
2022-01-03 17:23:00 -0500

    add reinstall on refresh

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # Install puppet-lint, using Puppet
 
 package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
-  name     => 'puppet-lint',
+  ensure               => '2.5.0',
+  provider             => 'gem',
+  reinstall_on_refresh => 'false',
 }
