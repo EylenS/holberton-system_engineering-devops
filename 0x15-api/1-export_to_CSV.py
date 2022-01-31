23740e08f4d4ac5c40a692a003179fe7c980589a
eylens <eymasac@gmail.com>
2022-01-31 14:51:42 -0500

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''This script uses a REST API, for a given employee ID,
+returns information about his/her TODO list progress.'''
+import requests
+from sys import argv
+import csv
+
+
+if __name__ == '__main__':
+    u_id = argv[1]
+    URL_name = 'https://jsonplaceholder.typicode.com/users/'
+    URL_todos = 'https://jsonplaceholder.typicode.com/todos?userId='
+
+    # Info about employee according to the id passed
+    dictUser = requests.get(URL_name + u_id).json()
+    employeeName = dictUser.get('name')
+
+    # List of dicts, info about tasks: userId, title, completed
+    dictTodos = requests.get(URL_todos + u_id).json()
+
+    user_data = []
+    for i in dictTodos:
+        if i['userId'] == int(u_id):
+            user_data.append(i)
+
+    data = []
+    for j in user_data:
+        data.append({'name': employeeName, 'id': u_id,
+                     'title': j['title'], 'completed': j['completed']})
+
+    # Writing to CSV files using the DictWriter class
+    fieldnames = ['id', 'name', 'completed', 'title']
+
+    with open('{}.csv'.format(u_id), 'w', encoding='UTF8', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames, quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+        for x in data:
+            writer.writerow(x)
