76daecfc3df62b781d0177a84ea111dd83811e77
eylens <eymasac@gmail.com>
2022-02-17 18:34:34 -0500

    How do I get hot posts from a subreddit.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' queries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit.'''
+import requests
+
+
+def top_ten(subreddit):
+    ''''''
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) \
+        Gecko/20100101 Firefox/96.0'
+    }
+    response = requests.get(url, headers=headers)
+
+    try:
+        res = response.json()
+        path = res['data']['children']
+        for idx in range(10):
+            print(path[idx]['data']['title'])
+    except:
+        return 0
