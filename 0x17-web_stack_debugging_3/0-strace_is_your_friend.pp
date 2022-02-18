f545a662e7017579219f13db85d16c4a24f6cd83
eylens <eymasac@gmail.com>
2022-02-18 16:29:21 -0500

    check: double_quoted_strings

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fix 500 error and Automating using Puppet
 exec { 'modify_file':
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => "/bin",
+  path    => '/bin',
 }
