4f920e97c646f6b3cffacc29320198dc725f1c07
eylens <eymasac@gmail.com>
2022-02-24 22:58:40 -0500

    includes the paths from where the executable command is stored

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # change open file allowed - ulimit
 exec { 'increase_ulimit':
   command => 'sed -i s/15/4096/g /etc/default/nginx; service nginx restart',
-  path    => '/bin',
+  path    => ['/bin', '/usr/bin', '/usr/bin'],
 }
